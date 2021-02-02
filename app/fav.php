42069bf00f132d19595f7bab2d462a5810a63d16
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2021-02-02 07:59:40 +0200

    Setup relationship and seeder

--- app/fav.php
+++ app/fav.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class fav extends Model
+class Fav extends Model
 {
-    //
+    protected   $table = 'favs';
+
+    /**
+     * Return the user owner for the favorite
+     */
+    public function user(){
+        return $this->belongsTo('users', 'user_id');
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Fav;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // Seed "favs" table
+
+        $users = User::get();
+        $faker = Faker\Factory::create();
+
+        // Add 10 favs to each user
+        foreach($users as $user){
+
+            for($i = 0; $i < 10; $i++){
+                $fav = new Fav();
+                
+                $fav->session_id = $faker->randomNumber(8);
+                $fav->fav_status = random_int(0, 10) % 2 == 0;
+                $fav->user_id = $user->id;
+
+                $fav->save();
+            }
+        }
     }
 }
